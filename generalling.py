c1a4a61c2bb0f8110ba16f98a86da23b80dfacbb
Marina Mazurova <sleepofnodreaming12@gmail.com>
2017-01-20 21:02:27 +0300

    generalling.py: docs improved.

--- generalling.py
+++ generalling.py
@@ -1,38 +1,52 @@
 import re
-from typing import Union, Tuple
+from typing import Union, Tuple, List
 
 import pymystem3
 
 GLOBAL_MYSTEM = pymystem3.Mystem()
 
 
+class NegationParser(object):
+
+    def __init__(self, negation_dict: List[str], ignoring_dict: Union[List[str], None]):
+        self._negation_dict = negation_dict
+        self._ignoring_dict = ignoring_dict
+        self._neg_cut = re.compile(r'\b(' + r'|'.join(self._negation_dict) + r')\b')
+        self._ignor_cut = None if self._ignoring_dict else re.compile(r'\b(' + r'|'.join(self._ignoring_dict) + r')\b')
+
+    @staticmethod
+    def cut_with_re(string, regex):
+        if regex is None:
+            return string
+        m = regex.match(string)
+        return string if not m else string[len(m.group(1)):]
+
+    def __call__(self, lemmas: list) -> list:
+        text, update_text = " ".join(lemmas), None
+        neg = True
+        while update_text != text:
+            if update_text is not None:
+                text = update_text
+            if neg:
+                update_text = self.cut_with_re(text, self._neg_cut).lstrip()
+                if update_text != text:
+                    neg = False
+            update_text = self.cut_with_re(update_text, self._ignor_cut).lstrip()
+        return update_text.split(), neg
+
+
 def parse_negations(lemmas: list, dictionary: list, ignored: Union[None, list]=None) -> Tuple[list, bool]:
     """
+    A deprecated wrapper for NegationParser factory.
 
-    :param lemmas:
-    :param dictionary:
-    :param ignored:
-    :return:
+    :param lemmas: A list of lemmas representing a text.
+    :param dictionary: A list of negations.
+    :param ignored: A list of words to ignore.
+
+    :return: A tuple of two: (a list of lemmas, flag whether a negation was detected and removed).
     """
-    text = " ".join(lemmas)
-
-    if ignored is not None:
-        for i in ignored:
-            if text.startswith(i):
-                text = text[len(i):].strip()
-                break
-
-    for neg in dictionary:
-        if text.startswith(neg + " "):
-            text, positive = text[len(neg) + 1:].strip(), False
-            break
-    else:
-        positive = True
-    if ignored is not None:
-        for i in ignored:
-            if text.startswith(i):
-                return text[len(i):].strip().split(), positive
-    return text.split(), positive
+    neg_parser = NegationParser(dictionary, ignored)
+    return neg_parser(lemmas)
 
 
 def pos(wd, analyzer=GLOBAL_MYSTEM):
