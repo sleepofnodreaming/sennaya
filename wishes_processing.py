54d55aae628fc5dbac3e4a01c49022e96105884d
Marina Mazurova <sleepofnodreaming12@gmail.com>
2017-01-28 16:39:30 +0300

    wishes_processing.py: a command line arg specifying a directory to save the results to added.

--- wishes_processing.py
+++ wishes_processing.py
@@ -3,9 +3,11 @@
 import csv
 import re
 import os
-from answer import Answer
-from like_processing import read_columns
+import time
 
+from answer import Answer
+from collections import namedtuple
+from like_processing import read_columns
 
 PARKING_TEMPLATES = [
     (r'\bподземный (стоянка|паркинг)\b', "устроить подземную парковку", lambda a, b: b),
@@ -268,23 +270,48 @@ class Postprocs(object):
                 pass
 
 
+OutputFiles = namedtuple("OutputFiles", ["clear", "questioned", "trash"])
+
+
 def iter_column(fn, col_number):
     return (Answer(text) for _, text in read_columns(fn, col_number))
+
+
+def generate_output_paths(directory=None):
+    time_string = time.strftime("%Y_%m_%d_%H_%M")
+    patterns = [
+        "output_clear_{}.csv",
+        "output_questioned_{}.csv",
+        "output_trash_{}.csv",
+    ]
+    names = map(lambda a: a.format(time_string), patterns)
+    if directory:
+        names = map(lambda a: os.path.join(directory, a), names)
+    return OutputFiles(*names)
 
 
 def parse_args():
     import argparse
     parser = argparse.ArgumentParser(description="A script classifying respondents' answers using a dictionary.")
-    parser.add_argument("csv", type=str, metavar="PATH", help="A path to a file to process.")
-    parser.add_argument("column", type=int, metavar="NUM", help="A number of column to process.")
-    parser.add_argument("dic", type=str, metavar="PATH", help="A path to a dictionary to use.")
+    parser.add_argument("csv", type=str, metavar="PATH", help="a path to a file to process")
+    parser.add_argument("column", type=int, metavar="NUM", help="a number of a column to get answers from")
+    parser.add_argument("dic", type=str, metavar="PATH", help="a path to a dictionary to use")
     parser.add_argument("-d", "--delimiter", type=str, default="\t", metavar="SYMBOL",
-                        help="A symbol to use as a delimiter in the output.")
+                        help="a symbol to use as a delimiter in the output")
+    parser.add_argument(
+        "-o", "--output", type=str, metavar="PATH",
+        help="a path to a directory to put the results to (by default they're saved to a dir where the script's located)"
+    )
     parsed = parser.parse_args()
     parsed.csv = os.path.expanduser(os.path.abspath(parsed.csv))
     parsed.dic = os.path.expanduser(os.path.abspath(parsed.dic))
+    if parsed.output is not None:
+        parsed.output = os.path.expanduser(os.path.abspath(parsed.output))
+
     assert parsed.column >= 0
-    assert os.path.isfile(parsed.csv) and os.path.isfile(parsed.dic)
+    assert os.path.isfile(parsed.csv)
+    assert os.path.isfile(parsed.dic)
+    assert parsed.output is None or os.path.isdir(parsed.output)
     assert len(parsed.delimiter) == 1
     return parsed
 
@@ -336,8 +363,10 @@ if __name__ == "__main__":
     header_tagging = sorted(all_tags)
     header = ["ID", "Исходный текст"] + header_tagging
 
-    with open("output_clear.csv", "w") as clear_file, open("output_questioned.csv", "w") as questioned_file, open(
-            "output_trash.csv", "w") as trash_file:
+    out_paths = generate_output_paths(args.output)
+
+    with open(out_paths.clear, "w") as clear_file, open(out_paths.questioned, "w") as questioned_file, open(
+            out_paths.trash, "w") as trash_file:
         c_writer = csv.writer(clear_file, delimiter=args.delimiter, quoting=csv.QUOTE_MINIMAL)
         q_writer = csv.writer(questioned_file, delimiter=args.delimiter, quoting=csv.QUOTE_MINIMAL)
         t_writer = csv.writer(trash_file, delimiter=args.delimiter, quoting=csv.QUOTE_MINIMAL)
